9a11e0ca89e9c3f3682943973dc37b3ed7499774
m1nj1m <lovekmj23@gmail.com>
2026-03-29 04:22:24 +0900

    ♻️ Refactor: Docker Compose 의존성 제거 및 DB 초기화 자동화
    spring-boot-docker-compose 의존성 제거
    PostgreSQL 초기 DB 생성 스크립트 추가 및 docker-compose 연동
    
    Related to: #21

---
+++ scripts/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE tenten_user;
+CREATE DATABASE tenten_hub;
+CREATE DATABASE tenten_company;
+CREATE DATABASE tenten_product;
+CREATE DATABASE tenten_order;
+CREATE DATABASE tenten_delivery;
+CREATE DATABASE tenten_ai;
