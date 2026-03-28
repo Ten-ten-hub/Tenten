ea4765188ecc648e8aa6ca4049407f4598c9722c
m1nj1m <lovekmj23@gmail.com>
2026-03-28 19:30:44 +0900

    🎉 Add: MSA 개발 환경 초기 세팅
    유레카 서버, API 게이트웨이, 샘플 서비스 기본 구성
    서비스 등록 및 게이트웨이 라우팅 확인
    
    Related to: #12

---
+++ sample-service/src/main/java/com/team/sampleservice/SampleServiceApplication.java
@@ -0,0 +1,11 @@
+package com.team.sampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServiceApplication.class, args);
+    }
+}
---
+++ sample-service/src/test/java/com/team/sampleservice/SampleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.sampleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
