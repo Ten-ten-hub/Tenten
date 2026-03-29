2a760a48ac219a5e1ce8c5f4a470f1b7922ab59f
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 05:51:20 +0900

    feature : User 도메인 에러코드 및 예외처리 추가

---
+++ user-service/src/main/java/com/team/userservice/global/domain/error/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.team.userservice.global.domain.error;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    HttpStatus getStatus();
+
+    String getCode();
+
+    String getMessage();
+}
+
