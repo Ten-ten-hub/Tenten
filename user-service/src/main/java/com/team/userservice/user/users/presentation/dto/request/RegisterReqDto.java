0f1a7083e239237582bae1018a6e0762b0ad6e77
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 05:53:58 +0900

    feature : User 도메인 Presentation 계층 구현

---
+++ user-service/src/main/java/com/team/userservice/user/users/presentation/dto/request/RegisterReqDto.java
@@ -0,0 +1,10 @@
+package com.team.userservice.user.users.presentation.dto.request;
+
+import com.team.userservice.user.core.enums.Role;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterReqDto(
+        @NotNull(message = "부여할 역할을 입력해주세요.")
+        Role giveRole
+) {
+}
