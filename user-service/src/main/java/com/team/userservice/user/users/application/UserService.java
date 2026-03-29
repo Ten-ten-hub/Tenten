b05875b2182f5b2bfad5d6e8e9cbd21b49b086ab
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 05:54:50 +0900

    feature : User 도메인 Application 계층 구현

---
+++ user-service/src/main/java/com/team/userservice/user/users/application/UserService.java
@@ -0,0 +1,12 @@
+package com.team.userservice.user.users.application;
+
+import com.team.userservice.user.core.enums.Role;
+import com.team.userservice.user.users.application.dto.SignUpResultDto;
+import com.team.userservice.user.users.application.dto.SignUpServiceDto;
+import java.util.UUID;
+
+public interface UserService {
+    SignUpResultDto signUp(SignUpServiceDto serviceDto);
+
+    void register(UUID userId, Role giveRole);
+}
