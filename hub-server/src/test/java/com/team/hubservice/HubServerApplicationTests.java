10930aad8ded8efd28bca89be4bd01951e485c31
hayeon <hayaon0603@naver.com>
2026-03-30 01:16:08 +0900

    🎉Add: Initialize hub server hub-server 초기 뼈대 구축 기본 패키지 및 설정 파일 추가 Related to: #23
    # Conflicts:
    #	hub-server/.gitignore
    #	hub-server/build.gradle
    #	hub-server/src/main/java/com/team/hubservice/HubServerApplication.java
    #	hub-server/src/main/resources/application.properties

---
+++ hub-server/src/test/java/com/team/hubservice/HubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.hubservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
