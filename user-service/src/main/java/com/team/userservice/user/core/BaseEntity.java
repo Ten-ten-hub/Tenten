19b99d3f98b7053e2d336608e23a7988e7cb6ac0
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 05:49:11 +0900

    feature : User 도메인 엔티티 작성

---
+++ user-service/src/main/java/com/team/userservice/user/core/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.team.userservice.user.core;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt; // 레코드 생성 시간
+
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy; // 레코드 생성자
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt; // 레코드 수정 시간
+
+    @LastModifiedBy
+    @Column(name = "updated_by", length = 100)
+    private String updatedBy; // 레코드 수정자
+
+
+}
