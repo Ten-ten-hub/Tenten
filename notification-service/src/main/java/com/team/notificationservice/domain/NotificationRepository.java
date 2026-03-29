cb8f358fa3fe823648e6acad921a568cd8d420aa
ji-circle <sp202401jw@gmail.com>
2026-03-30 04:04:57 +0900

    Feat: 알림 내역 검색 및 페이징 조회 API 구현

--- notification-service/src/main/java/com/team/notificationservice/domain/NotificationRepository.java
+++ notification-service/src/main/java/com/team/notificationservice/domain/NotificationRepository.java
@@ -1,7 +1,14 @@
 package com.team.notificationservice.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    Page<Notification> findByReceiverSlackIdAndDeletedAtIsNull(String slackId, Pageable pageable);
+
+    Page<Notification> findByReceiverSlackIdAndMsgContentContainingAndDeletedAtIsNull(
+            String slackId, String msgContent, Pageable pageable);
 }
