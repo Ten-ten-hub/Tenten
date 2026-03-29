2e5f6beab3e04ee8e2e3fc98ec892423376bd6ea
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 05:54:50 +0900

    feature : User 도메인 Application 계층 구현

---
+++ user-service/src/main/java/com/team/userservice/user/users/application/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.team.userservice.user.users.application;
+
+import com.team.userservice.global.domain.error.UserErrorCode;
+import com.team.userservice.global.exception.UserException;
+import com.team.userservice.user.core.User;
+import com.team.userservice.user.core.enums.Role;
+import com.team.userservice.user.users.application.dto.SignUpResultDto;
+import com.team.userservice.user.users.application.dto.SignUpServiceDto;
+import com.team.userservice.user.users.domain.UserRepository;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public SignUpResultDto signUp(SignUpServiceDto serviceDto) {
+
+        if (userRepository.existsByLoginId(serviceDto.loginId())) {
+            throw new UserException(UserErrorCode.DUPLICATE_LOGIN_ID);
+        }
+        if (userRepository.existsByEmail(serviceDto.email())) {
+            throw new UserException(UserErrorCode.DUPLICATE_EMAIL);
+        }
+
+        User user = userRepository.save(User.create()
+                .loginId(serviceDto.loginId())
+                .password(serviceDto.password())
+                .name(serviceDto.name())
+                .role(serviceDto.role())
+                .slackId(serviceDto.slackId())
+                .email(serviceDto.email())
+                .phoneNumber(serviceDto.phoneNumber())
+                .build());
+
+        return SignUpResultDto.from(user);
+    }
+
+    @Override
+    @Transactional
+    public void register(UUID userId, Role giveRole) {
+        userRepository.register(userId, giveRole);
+    }
+}
