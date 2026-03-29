50a6e76d794421dedada218c0181100e7541a9bd
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 05:49:11 +0900

    feature : User 도메인 엔티티 작성

---
+++ user-service/src/main/java/com/team/userservice/user/core/HubUser.java
@@ -0,0 +1,27 @@
+package com.team.userservice.user.core;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "p_hub_user")
+public class HubUser extends BaseEntity{
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    @UuidGenerator
+    private UUID id;
+
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    @OneToOne(fetch = FetchType.LAZY) // 물리 FK
+    private User userId;
+
+    @Column(name = "hub_id", nullable = false) // 논리 FK // 유니크일 필요 없음
+    private UUID hubId;
+}
