938851104f6017cdeee1938c344a11d87f64a860
hayeon <hayaon0603@naver.com>
2026-03-30 15:14:10 +0900

    ♻️Refactor: Apply common BaseEntity to Hub domain 공통 모듈의 BaseEntity를 상속받도록 허브 엔티티를 수정하여 중복 코드를 제거함 JPA Auditing 및 Soft Delete 필드가 BaseEntity로 이관됨에 따라 관련 로직을 정리함 삭제 처리 시 사용되는 식별자 타입을 String에서 UUID로 변경하여 서비스 및 컨트롤러 로직에 반영함 Related to: #23, #24

--- hub-server/src/main/java/com/team/hubservice/HubServerApplication.java
+++ hub-server/src/main/java/com/team/hubservice/HubServerApplication.java
@@ -3,6 +3,7 @@ package com.team.hubservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HubServerApplication {
 
