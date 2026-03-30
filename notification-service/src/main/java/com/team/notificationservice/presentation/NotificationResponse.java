d392ec799524c9e740afebd66f9ab90475abdfe1
ji-circle <sp202401jw@gmail.com>
2026-03-30 14:16:41 +0900

    refactor : DTO를 Java Record로 리팩터링

--- notification-service/src/main/java/com/team/notificationservice/presentation/NotificationResponse.java
+++ notification-service/src/main/java/com/team/notificationservice/presentation/NotificationResponse.java
@@ -5,22 +5,20 @@ import com.team.notificationservice.domain.SendStatus;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-public class NotificationResponse {
-    private UUID id;
-    private String message;
-    private SendStatus status;
-    private LocalDateTime createdAt;
-
+public record NotificationResponse(
+    UUID id,
+    String message,
+    SendStatus status,
+    LocalDateTime createdAt
+) {
     public static NotificationResponse from(Notification notification) {
         return NotificationResponse.builder()
-                .id(notification.getId())
-                .message(notification.getMsgContent())
-                .status(notification.getSendStatus())
-                .createdAt(notification.getCreatedAt())
-                .build();
+            .id(notification.getId())
+            .message(notification.getMsgContent())
+            .status(notification.getSendStatus())
+            .createdAt(notification.getCreatedAt())
+            .build();
     }
 }
