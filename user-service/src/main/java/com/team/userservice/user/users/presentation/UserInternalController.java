ba10458568a0ef5c0f3de3644b4d9806748178ed
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 16:47:31 +0900

    refactor : 로그인전용 내부 api 수정

--- user-service/src/main/java/com/team/userservice/user/users/presentation/UserInternalController.java
+++ user-service/src/main/java/com/team/userservice/user/users/presentation/UserInternalController.java
@@ -1,12 +1,11 @@
 package com.team.userservice.user.users.presentation;
 
 import com.team.userservice.user.users.application.UserService;
-import com.team.userservice.user.users.presentation.dto.request.LoginReqDto;
 import com.team.userservice.user.users.presentation.dto.response.LoginResDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
