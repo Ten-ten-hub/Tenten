34a193101c0d88fa49eaa24802768eb6f50d3c80
ji-circle <sp202401jw@gmail.com>
2026-03-30 14:16:41 +0900

    refactor : DTO를 Java Record로 리팩터링

--- notification-service/src/main/java/com/team/notificationservice/application/NotificationRequest.java
+++ notification-service/src/main/java/com/team/notificationservice/application/NotificationRequest.java
@@ -2,17 +2,12 @@ package com.team.notificationservice.application;
 
 import com.team.notificationservice.domain.MsgType;
 import java.util.UUID;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class NotificationRequest {
-    private String receiverSlackId;
-    private String email;
-    private UUID orderId;
-    private String message;
-    private MsgType msgType;
+public record NotificationRequest(
+    String receiverSlackId,
+    String email,
+    UUID orderId,
+    String message,
+    MsgType msgType
+) {
 }
--- notification-service/src/main/java/com/team/notificationservice/application/NotificationSearchCondition.java
+++ notification-service/src/main/java/com/team/notificationservice/application/NotificationSearchCondition.java
@@ -1,11 +1,7 @@
 package com.team.notificationservice.application;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class NotificationSearchCondition {
-    private String slackId;
-    private String keyword; // 메시지 내용 검색용 키워드 추가
+public record NotificationSearchCondition(
+    String slackId,
+    String keyword
+) {
 }
