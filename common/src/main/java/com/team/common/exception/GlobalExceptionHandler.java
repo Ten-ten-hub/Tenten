c5c70852d2ed02c187925e6cafa816f8c698bded
rlaxxwls13 <rlaxxwls13@hufs.ac.kr>
2026-03-30 21:49:56 +0900

    Feat: 일반 Exception에 대한 핸들러 추가
    Related to: #32

--- common/src/main/java/com/team/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/com/team/common/exception/GlobalExceptionHandler.java
@@ -2,13 +2,14 @@ package com.team.common.exception;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -26,6 +27,13 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(CommonErrorCode.INVALID_INPUT.getStatus())
             .body(ErrorResponse.of(CommonErrorCode.INVALID_INPUT, details));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unexpected error occurred", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorResponse.of(CommonErrorCode.INTERNAL_SERVER_ERROR));
     }
 
 }
