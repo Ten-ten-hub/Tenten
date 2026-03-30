e98162bf6730846f0031e9a717e6f4c52da7b1d2
ji-circle <sp202401jw@gmail.com>
2026-03-30 14:16:03 +0900

    feat: 커스텀 에러코드 및 전역 예외 처리 구현

---
+++ notification-service/src/main/java/com/team/notificationservice/presentation/common/ServiceException.java
@@ -0,0 +1,14 @@
+package com.team.notificationservice.presentation.common;
+
+public class ServiceException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public ServiceException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
