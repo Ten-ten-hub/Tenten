c4cbd8b9e3a06cffc0694593fcbe41cd1115cf50
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 16:47:31 +0900

    refactor : 로그인전용 내부 api 수정

--- user-service/src/main/java/com/team/userservice/user/users/presentation/UserInternalController.java
+++ user-service/src/main/java/com/team/userservice/user/users/presentation/UserInternalController.java
@@ -2,10 +2,11 @@ package com.team.userservice.user.users.presentation;
 
 import com.team.userservice.global.dto.CommonResponse;
 import com.team.userservice.user.users.application.UserService;
+import com.team.userservice.user.users.presentation.dto.request.LoginReqDto;
 import com.team.userservice.user.users.presentation.dto.response.LoginResDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInternalController {
     private final UserService userService;
 
-    @GetMapping("/users/login-id/{loginId}")
-    public CommonResponse<LoginResDto> login(@PathVariable("loginId") String loginId) {
-        return CommonResponse.onSuccess(LoginResDto.from(userService.findByLoginId(loginId)));
+    @PostMapping("/users/login-id/{loginId}")
+    public CommonResponse<LoginResDto> login(@RequestBody LoginReqDto loginRequest) {
+        return CommonResponse.onSuccess(
+                LoginResDto.from(userService.loginService(loginRequest.loginId(), loginRequest.password())));
     }
 }
