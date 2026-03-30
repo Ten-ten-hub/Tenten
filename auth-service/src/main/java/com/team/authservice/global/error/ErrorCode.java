eb5ad08d9f6e83a01ad906eab8586d5c2187dc95
woongcheol Na <32201345@dankook.ac.kr>
2026-03-30 22:54:06 +0900

    feature : auth-service 에러코드, 기본 애플리케이션, 공통 응답

---
+++ auth-service/src/main/java/com/team/authservice/global/error/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.team.authservice.global.error;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+
+    HttpStatus getStatus();
+
+    String getCode();
+
+    String getMessage();
+}
+
