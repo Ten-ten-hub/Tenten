e344be90b3732f1fe33b445647ee0ac56f392769
woongcheol Na <32201345@dankook.ac.kr>
2026-03-31 15:03:21 +0900

    fix: user-service 피드백 반영

--- user-service/src/main/java/com/team/userservice/global/domain/error/UserErrorCode.java
+++ user-service/src/main/java/com/team/userservice/global/domain/error/UserErrorCode.java
@@ -13,7 +13,8 @@ public enum UserErrorCode implements ErrorCode {
     DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "USER_409_1", "이미 사용 중인 아이디입니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "USER_409_2", "이미 사용 중인 이메일입니다."),
     ALREADY_REGISTERED_USER(HttpStatus.CONFLICT, "USER_409_3", "이미 등록이 완료된 유저입니다."),
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "USER_401", "아이디 또는 비밀번호가 일치하지 않습니다.");
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "USER_401", "아이디 또는 비밀번호가 일치하지 않습니다."),
+    DUPLICATE_USER_INFO(HttpStatus.CONFLICT, "USER_409_4", "이미 사용 중인 정보입니다.");
 
     private final HttpStatus status;
     private final String code;
