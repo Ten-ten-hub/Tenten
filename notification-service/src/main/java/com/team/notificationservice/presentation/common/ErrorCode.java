c6962fca622e8aaa38263c97b79f351c17a8b9b4
ji-circle <sp202401jw@gmail.com>
2026-03-31 16:58:44 +0900

    refactor: 수신자 미존재 에러 코드 상태 변경(404)

--- notification-service/src/main/java/com/team/notificationservice/presentation/common/ErrorCode.java
+++ notification-service/src/main/java/com/team/notificationservice/presentation/common/ErrorCode.java
@@ -12,9 +12,9 @@ public enum ErrorCode {
     COMMON_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_SERVER_ERROR", "서버 내부 오류가 발생했습니다."),
 
     // 알림 서비스 전용 에러
-    NOTI_NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND_NOTIFICATION", "해당 알림을 찾을 수 없거나 이미 삭제되었습니다."),
-    NOTI_RECIPIENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "NOT_FOUND_RECIPIENT", "알림 수신 대상자(슬랙 ID 또는 이메일)를 찾을 수 없습니다."),
-    NOTI_SLACK_API_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "SLACK_API_ERROR", "슬랙 메시지 전송 중 외부 서비스 오류가 발생했습니다.");
+    NOTI_RECIPIENT_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI_RECIPIENT_NOT_FOUND", "알림 수신 대상자(슬랙 ID 또는 이메일)를 찾을 수 없습니다."),
+    NOTI_NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI_NOT_FOUND", "해당 알림 정보를 찾을 수 없습니다."),
+    NOTI_SLACK_API_ERROR(HttpStatus.SERVICE_UNAVAILABLE, "NOTI_SLACK_ERROR", "슬랙 API 호출 중 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
