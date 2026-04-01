22197908556a556544384ae8d7ee47e972e68c8d
ji-circle <sp202401jw@gmail.com>
2026-04-01 17:44:41 +0900

    refactor: 상속 및 Handler 이름 변경

--- notification-service/src/main/java/com/team/notificationservice/presentation/common/ServiceException.java
+++ notification-service/src/main/java/com/team/notificationservice/presentation/common/ServiceException.java
@@ -1,38 +1,39 @@
 package com.team.notificationservice.presentation.common;
 
+import com.team.common.exception.BusinessException;
 import java.util.List;
+import lombok.Getter;
 
-public class ServiceException extends RuntimeException {
+public class ServiceException extends BusinessException {
     private final ErrorCode errorCode;
     // 상세 에러 정보를 담을 리스트 추가
+    @Getter
     private final List<ApiResponse.ValidationError> errors;
 
     // 기존 생성자 (상세 에러가 없는 경우)
     public ServiceException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
+        super(errorCode);
         this.errorCode = errorCode;
         this.errors = null;
     }
 
     // 새로운 생성자 (상세 에러가 있는 경우)
     public ServiceException(ErrorCode errorCode, List<ApiResponse.ValidationError> errors) {
-        super(errorCode.getMessage());
+        super(errorCode);
         this.errorCode = errorCode;
         this.errors = errors;
     }
 
     // 원인 보존을 위한 생성자 추가
     public ServiceException(ErrorCode errorCode, Throwable cause) {
-        super(errorCode.getMessage(), cause);
+        super(errorCode);
         this.errorCode = errorCode;
         this.errors = null;
     }
 
+    @Override
     public ErrorCode getErrorCode() {
         return errorCode;
     }
 
-    public List<ApiResponse.ValidationError> getErrors() {
-        return errors;
-    }
 }
