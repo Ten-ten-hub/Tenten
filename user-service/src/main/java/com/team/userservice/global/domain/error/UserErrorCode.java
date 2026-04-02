962665b4d3add77915623d4cafe926f274f1ae65
woongcheol Na <32201345@dankook.ac.kr>
2026-04-02 14:46:36 +0900

    feat: user-service _ 에러코드 추가

--- user-service/src/main/java/com/team/userservice/global/domain/error/UserErrorCode.java
+++ user-service/src/main/java/com/team/userservice/global/domain/error/UserErrorCode.java
@@ -14,7 +14,9 @@ public enum UserErrorCode implements ErrorCode {
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "USER_409_2", "이미 사용 중인 이메일입니다."),
     ALREADY_REGISTERED_USER(HttpStatus.CONFLICT, "USER_409_3", "이미 등록이 완료된 유저입니다."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "USER_401", "아이디 또는 비밀번호가 일치하지 않습니다."),
-    DUPLICATE_USER_INFO(HttpStatus.CONFLICT, "USER_409_4", "이미 사용 중인 정보입니다.");
+    DUPLICATE_USER_INFO(HttpStatus.CONFLICT, "USER_409_4", "이미 사용 중인 정보입니다."),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "USER_400", "요청 값이 올바르지 않습니다."),
+    ROLE_AFFILIATION_CONFLICT(HttpStatus.BAD_REQUEST, "USER_400", "권한과 소속이 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String code;
