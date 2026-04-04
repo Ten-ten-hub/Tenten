c91961f50b43b0808dc64ddd0174717bdc4194a7
woongcheol Na <32201345@dankook.ac.kr>
2026-04-05 06:44:30 +0900

    refactor: auth-service 공통모듈 적용

--- auth-service/src/main/java/com/team/authservice/global/exception/AuthException.java
+++ auth-service/src/main/java/com/team/authservice/global/exception/AuthException.java
@@ -1,14 +1,12 @@
 package com.team.authservice.global.exception;
 
 import com.team.authservice.global.error.AuthErrorCode;
+import com.team.common.exception.BusinessException;
 import lombok.Getter;
 
 @Getter
-public class AuthException extends RuntimeException {
-    private final AuthErrorCode errorCode;
-
+public class AuthException extends BusinessException {
     public AuthException(AuthErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+        super(errorCode);
     }
 }
