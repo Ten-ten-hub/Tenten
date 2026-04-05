e611a67b9e152e4bbb2881ee3e793a15b11c8b30
ji-circle <sp202401jw@gmail.com>
2026-04-06 03:53:31 +0900

    fix: test 프로필 활성화

--- ai-service/src/test/java/com/team/aiservice/AiServiceApplicationTests.java
+++ ai-service/src/test/java/com/team/aiservice/AiServiceApplicationTests.java
@@ -2,12 +2,12 @@ package com.team.aiservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AiServiceApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
